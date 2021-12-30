0d146a975c3c6adbe3998af715d3f5bc0e32243e
buitongminhchau01032002 <buitongminhchau01032002@gmail.com>
2021-12-30 21:05:58 +0700

    change port in app

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const { createCanvas, registerFont } = require('canvas')
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(function(req, res, next) {
