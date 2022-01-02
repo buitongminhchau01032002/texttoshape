756182c342faa9893767020b1bfc692aff02053c
buitongminhchau01032002 <buitongminhchau01032002@gmail.com>
2022-01-02 12:45:02 +0700

    test fixbug

--- app.js
+++ app.js
@@ -207,8 +207,8 @@ function makeHeartImage (stringInput) {
             let x = (x2+x1)/2 - ctx.measureText(stringDraw).width/2
             ctx.fillText(stringDraw, x, y)
         } else if (arrWord.length > 0) {
-            // let spaceWidth = ((x2-x1) - ctx.measureText(stringDraw.replaceAll(' ', '')).width)/(arrWord.length-1)
-            // let xCurrent = x1
+            let spaceWidth = ((x2-x1) - ctx.measureText(stringDraw.replaceAll(' ', '')).width) / (arrWord.length-1)
+            let xCurrent = x1
             // arrWord.forEach(word => {
             //     ctx.fillText(word, xCurrent, y)
             //     xCurrent += (ctx.measureText(word).width + spaceWidth)
